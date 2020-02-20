5c6c3c764da3e505d2cc1c2e4456337ef5b6e8d2
mangojuicedeli <mgkang0317@gmail.com>
2020-02-20 16:34:12 +0900

    롬복 기능 테스트 완료

---
+++ src/main/java/com/mingu/pjt/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.mingu.pjt.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
