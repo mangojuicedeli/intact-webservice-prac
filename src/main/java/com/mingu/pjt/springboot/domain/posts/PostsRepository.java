ef29dc73f2551c7658ffd6ed0b69102eda272749
mangojuicedeli <mgkang0317@gmail.com>
2020-02-21 16:16:20 +0900

    JPA 및 H2 기능 설정 및 테스트 완료

---
+++ src/main/java/com/mingu/pjt/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.mingu.pjt.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> { }
