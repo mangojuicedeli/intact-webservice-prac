6065ad6cc050e90dc39eda00c6ccbc1f93d4b790
mangojuicedeli <mgkang0317@gmail.com>
2020-02-27 12:35:25 +0900

    전체 조회 화면 만들기

--- src/main/java/com/mingu/pjt/springboot/service/posts/PostsService.java
+++ src/main/java/com/mingu/pjt/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mingu.pjt.springboot.service.posts;
 
 import com.mingu.pjt.springboot.domain.posts.Posts;
 import com.mingu.pjt.springboot.domain.posts.PostsRepository;
+import com.mingu.pjt.springboot.web.dto.PostsListResponseDto;
 import com.mingu.pjt.springboot.web.dto.PostsResponseDto;
 import com.mingu.pjt.springboot.web.dto.PostsSaveRequestDto;
 import com.mingu.pjt.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,4 +43,13 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
