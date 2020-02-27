337ff56a7bd294b849fb00edb8291dceee474bce
mangojuicedeli <mgkang0317@gmail.com>
2020-02-27 23:36:06 +0900

    게시글 삭제 만들기

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -44,7 +44,22 @@ var main = {
         }).done(function() {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
-        }).fail(function() {
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
             alert(JSON.stringify(error));
         });
     }
