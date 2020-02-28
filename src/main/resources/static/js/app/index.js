3bf78c753546a54331ca17e632cff72da1311600
mangojuicedeli <mgkang0317@gmail.com>
2020-02-28 18:10:18 +0900

    구글 서비스 등록

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,11 +1,15 @@
 var main = {
     init : function () {
         var _this = this;
+
         $('#btn-save').on('click', function () {
             _this.save();
         });
         $('#btn-update').on('click', function () {
             _this.update();
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
         });
     },
     save : function () {
