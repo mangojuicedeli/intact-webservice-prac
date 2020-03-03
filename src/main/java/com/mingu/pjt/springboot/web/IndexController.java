a2968d292035ccc944107e8939014f2891a6f5b7
mangojuicedeli <mgkang0317@gmail.com>
2020-03-03 14:30:36 +0900

    스프링 시큐리티 구글 로그인 테스트 완료

--- src/main/java/com/mingu/pjt/springboot/web/IndexController.java
+++ src/main/java/com/mingu/pjt/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.mingu.pjt.springboot.web;
 
+import com.mingu.pjt.springboot.config.auth.dto.SessionUser;
+import com.mingu.pjt.springboot.domain.user.User;
 import com.mingu.pjt.springboot.service.posts.PostsService;
 import com.mingu.pjt.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) { model.addAttribute("userName", user.getName()); }
         return "index";
     }
 
